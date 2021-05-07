9452300a60991951f127b598c0235f3df66a62a8
Tiago Gomes <tiagogomes187@gmail.com>
2021-05-07 16:52:53 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -24,12 +24,7 @@ const DonutChart = () => {
       setCharData({ labels: myLabels, series: mySeries });
     });
   }, []);
-
-  //const mockData = {
-  //    series: [477138, 499928, 444867, 220426, 473088],
-  //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-  //}
-
+  
   const options = {
     legend: {
       show: true,
--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,10 @@
 export type SaleSum = {
-    sallerName: string,
-    sum: number
+    sallerName: string;
+    sum: number;
+}
+
+export type SaleSuccess = {
+    sallerName: string;
+    visited: number;
+    deals: number;
 }
