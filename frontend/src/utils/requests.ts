dd4336b7faa72d6e7c3f9cbec4d03624fe0feccd
Tiago Gomes <tiagogomes187@gmail.com>
2021-05-08 15:21:16 -0300

    Fiz alterações no 'requests.ts'.

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://sds3-tiago.herokuapp.com';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
